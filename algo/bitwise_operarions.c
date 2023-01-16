fa52304957cc0622eb9440f07847b6e7b65005d1
veeresh07 <vmathapati56@gmail.com>
2023-01-17 00:08:42 +0530

    algo-add bitwise operation code

---
+++ algo/bitwise_operarions.c
@@ -0,0 +1,39 @@
+// C program to set, clear and toggle a bit
+
+#include <stdio.h>
+
+// Function to set the kth bit of n
+int setBit(int n, int k)
+{
+	return (n | (1 << (k - 1)));
+}
+
+// Function to clear the kth bit of n
+int clearBit(int n, int k)
+{
+	return (n & (~(1 << (k - 1))));
+}
+
+// Function to toggle the kth bit of n
+int toggleBit(int n, int k)
+{
+	return (n ^ (1 << (k - 1)));
+}
+
+// Driver code
+int main()
+{
+	int n = 0, k = 0;
+
+	printf("enetr number: ");
+	scanf("%d",&n);
+	printf("enetr bit position to be changed: ");
+	scanf("%d",&k);
+	printf("%d with %d-th bit Set: %d\n",
+		n, k, setBit(n, k));
+	printf("%d with %d-th bit Cleared: %d\n",
+		n, k, clearBit(n, k));
+	printf("%d with %d-th bit Toggled: %d\n",
+		n, k, toggleBit(n, k));
+	return 0;
+}
